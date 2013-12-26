272840c0a49c39f927a087e1f758661e5ece8b6b
Surtri <devstrich@hotmail.com>
2013-12-25 22:49:48 -0500

    Update articles.php

--- articles.php
+++ articles.php
@@ -1,8 +1,8 @@
 <?php
 $articles = array(
 	array(
-	'title' => 'This is my first article',
-	'subtitle' => 'This is my first subtitle',
+	'title' => 'Aoft Vs. The Web',
+	'subtitle' => '',
 	'datemaj' => 'jan',
 	'datemin' => 'uary',
 	'day' => '01',
@@ -10,7 +10,16 @@ $articles = array(
 	'imgurl' => '',
 	'socials' => array('0','1','2','3'),
 	//'' => '',
-	'body' => "We'll call that a practice run..."),
+	'body' => "It’s not often I can properly explain the sort of shit I have to put up with constantly, but I finally found a video that can do it:
+http://www.youtube.com/watch?v=W8_Kfjo3VjU
+
+The saddest part about this is after showing my friends, a large percentage still didn’t get it.
+
+"You don't know the difference between our website and the internet?"
+
+Sometimes I wonder how these people found the internet at all without accidentally setting their computer on fire.
+ I don’t claim to be a professional, but people lacking basic computer skills seem to be growing impossibly fast in such a technological age. I’m not even sure how it’s possible…
+"),
 	array(
 	'title' => 'This is my first article',
 	'subtitle' => 'This is my first subtitle',
