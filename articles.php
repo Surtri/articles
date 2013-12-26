97f4c3178f6ca5e7cae86a32fa83153c1cc9d5e2
CosmoHill <cosmohill496@gmail.com>
2013-12-26 04:07:34 +0000

    Update articles.php

--- articles.php
+++ articles.php
@@ -10,12 +10,12 @@ $articles = array(
 	'imgurl' => '',
 	'socials' => array('0','1','2','3'),
 	//'' => '',
-	'body' => "It’s not often I can properly explain the sort of shit I have to put up with constantly, but I finally found a video that can do it:
+	'body' => "It is not often I can properly explain the sort of shit I have to put up with constantly, but I finally found a video that can do it:
 http://www.youtube.com/watch?v=W8_Kfjo3VjU
 
 The saddest part about this is after showing my friends, a large percentage still didn’t get it.
 
-"You don't know the difference between our website and the internet?"
+"You dont know the difference between our website and the internet?"
 
 Sometimes I wonder how these people found the internet at all without accidentally setting their computer on fire.
  I don’t claim to be a professional, but people lacking basic computer skills seem to be growing impossibly fast in such a technological age. I’m not even sure how it’s possible…
